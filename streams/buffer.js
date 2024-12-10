65c4f5b15d9440c9d04df0eaf7ef09c15bc5bb8d
Massaroni <49594702+Massaronii@users.noreply.github.com>
2024-12-10 14:37:47 -0300

    Add files via upload

---
+++ streams/buffer.js
@@ -0,0 +1,7 @@
+//buffer é uma representação de dados na memoria em baixo nivel que o node utiliza, podemos utilizar e ler eles
+
+const buf = Buffer.from("hello")
+
+console.log(buf)
+
+console.log(buff.toJSON())
